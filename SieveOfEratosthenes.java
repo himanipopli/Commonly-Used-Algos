267730e67fa55012ad96ccce817b3f4430e75e89
Himani Popli <44602222+himanipopli@users.noreply.github.com>
2019-10-18 14:41:20 +0530

    Create SieveOfEratosthenes.java

---
+++ SieveOfEratosthenes.java
@@ -0,0 +1,38 @@
+package commonAlgos;
+import java.util.*;
+
+public class SieveOfEratosthenes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int t=scan.nextInt();
+		for(int i=0;i<t;i++) {
+			int n=scan.nextInt();
+			sieve(n);
+		}
+	}
+	
+	public static void sieve(int n) 
+    { 
+        boolean prime[] = new boolean[n+1]; 
+        for(int i=0;i<=n;i++) 
+            prime[i] = true; 
+          
+        for(int p = 2; p*p <=n; p++) 
+        { 
+            if(prime[p] == true) 
+            { 
+                for(int i = p*p; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        } 
+          
+        for(int i = 2; i <= n; i++) 
+        { 
+            if(prime[i] == true) 
+                System.out.print(i + " "); 
+        } 
+    } 
+
+}
